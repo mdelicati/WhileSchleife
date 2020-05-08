d44b389c1cfc744153f0be6b60529a5b01452cd1
mdelicati <marco.delicati@evopro-ag.de>
2020-05-08 10:46:34 +0200

    Add project files.

---
+++ WhileSchleife/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WhileSchleife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 50)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
